13fe4f2dad9b54c40c91a2e97f2c46581b25f9bc
BlackWhiteYoshi <btoelke@mail.uni-paderborn.de>
2020-07-26 11:30:36 +0200

    - refactor input component   - remove bool parameter Password and instead using string parameter type   - add parameter for "id" and "for"   - add parameter for "name"   - add parameter for "autocomplete"

--- NoJsBlazor/Input/Input.razor.cs
+++ NoJsBlazor/Input/Input.razor.cs
@@ -20,19 +20,52 @@ namespace NoJsBlazor {
         public EventCallback<string> ValueChanged { get; set; }
 
         /// <summary>
+        /// <para>sets properties Label, Id, Name and Autocomplete to the given string (Autocomplete to true)</para>
+        /// <para>returns a comma seperated string with this 4 properties (Label,Id,Name,Autocomplete)</para>
+        /// </summary>
+        [Parameter]
+        public string Title {
+            get => $"{Label},{Id},{Name},{Autocomplete}";
+            set {
+                Label = value;
+                Id = value;
+                Name = value;
+                Autocomplete = true;
+            }
+        }
+
+        /// <summary>
         /// Displayed text of this Input field.
         /// </summary>
         [Parameter]
-        public string Name { get; set; }
+        public string Label { get; set; }
 
         /// <summary>
         /// Indicates if the characters should be display as *****
         /// </summary>
         [Parameter]
-        public bool Password { get; set; }
+        public string Type { get; set; }
 
         /// <summary>
-        /// Captures unmatched values
+        /// sets the "id"-attribute in input field and the "for"-attribute in the label.
+        /// </summary>
+        [Parameter]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// sets the "name"-attribute in the input field.
+        /// </summary>
+        [Parameter]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// sets the "autocomplete"-attribute in the input field.
+        /// </summary>
+        [Parameter]
+        public bool Autocomplete { get; set; }
+
+        /// <summary>
+        /// Captures unmatched values. The values are applied to the outer div container and not to the input field
         /// </summary>
         [Parameter(CaptureUnmatchedValues = true)]
         public Dictionary<string, object> Attributes { get; set; }
