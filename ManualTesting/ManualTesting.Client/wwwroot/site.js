7efb28804e41fc2d8d8684d9a0674a23632d6f0f
BlackWhiteYoshi <bastian@firerocket.de>
2023-04-01 19:44:00 +0200

    - moved inline js-script to file - changed text to english

---
+++ ManualTesting/ManualTesting.Client/wwwroot/site.js
@@ -0,0 +1,7 @@
+function setPointerCapture(targetElement, pointerId) {
+    targetElement.setPointerCapture(pointerId);
+}
+
+function releasePointerCapture(targetElement, pointerId) {
+    targetElement.releasePointerCapture(pointerId);
+}
