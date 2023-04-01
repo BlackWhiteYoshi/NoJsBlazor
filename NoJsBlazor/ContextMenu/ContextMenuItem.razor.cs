ada7f57d55736e9a2d51757ed29d6f4f5d895247
BlackWhiteYoshi <bastian@firerocket.de>
2023-04-01 14:02:44 +0200

    in ContextSubMenu: changed EventArgs to MouseEventArgs

--- NoJsBlazor/ContextMenu/ContextMenuItem.razor.cs
+++ NoJsBlazor/ContextMenu/ContextMenuItem.razor.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// <para>Wrapper for the content that will be a context item.</para>
-/// <para>This should be placed inside of a <see cref="ContextMenu"/>/<see cref="ContextSubMenu"/> instance.</para>
+/// <para>This should be placed inside of a <see cref="ContextMenu"/> or <see cref="ContextSubMenu"/> instance.</para>
 /// </summary>
 public sealed partial class ContextMenuItem : ComponentBase {
     /// <summary>
@@ -12,12 +12,11 @@ public sealed partial class ContextMenuItem : ComponentBase {
     public required RenderFragment ChildContent { get; set; }
 
     /// <summary>
-    /// <para>Invokes every time when this list item get clicked or touched.</para>
-    /// <para><see cref="EventArgs"/> is either <see cref="MouseEventArgs"/> or <see cref="TouchEventArgs"/>, depends if it was invoked by clicking or touching.</para>
+    /// Invokes every time when this list item get clicked or touched.
     /// </summary>
     [Parameter]
-    public EventCallback<EventArgs> OnPressed { get; set; }
+    public EventCallback<MouseEventArgs> OnPressed { get; set; }
 
 
-    private void OnClick(EventArgs e) => OnPressed.InvokeAsync(e);
+    private void OnClick(MouseEventArgs e) => OnPressed.InvokeAsync(e);
 }
