a8568b87193d8729cffbbcfa82c84d2b5f914289
BlackWhiteYoshi <bastian@firerocket.de>
2023-11-04 08:20:36 +0100

    in ManualTesting: added @ts-check to js-file

--- ManualTesting/ManualTesting.Client/wwwroot/site.js
+++ ManualTesting/ManualTesting.Client/wwwroot/site.js
@@ -1,7 +1,18 @@
+// @ts-check
+
+
+/**
+ * @param {HTMLObjectElement} targetElement
+ * @param {number} pointerId
+ */
 function setPointerCapture(targetElement, pointerId) {
     targetElement.setPointerCapture(pointerId);
 }
 
+/**
+ * @param {HTMLObjectElement} targetElement
+ * @param {number} pointerId
+ */
 function releasePointerCapture(targetElement, pointerId) {
     targetElement.releasePointerCapture(pointerId);
 }
